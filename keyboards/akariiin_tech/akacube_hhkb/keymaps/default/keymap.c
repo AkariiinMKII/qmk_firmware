c8c05fb22b56986c62be7ac8c133fb160f412cec
AkariiinMKII <6019344+AkariiinMKII@users.noreply.github.com>
2024-11-06 20:56:08 +0800

    add mouse keys

--- keyboards/akariiin_tech/akacube_hhkb/keymaps/default/keymap.c
+++ keyboards/akariiin_tech/akacube_hhkb/keymaps/default/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
-                          KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI, KC_RALT
+                          KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI, KC_RALT, MS_BTN1, MS_BTN2
     ),
 
     /*
@@ -43,6 +43,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_UP,   _______, _______,
         _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,          KC_PENT,
         _______,          _______, _______, _______, _______, _______, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, _______, _______,
-                          _______, _______,                   _______,                            _______, _______
+                          _______, _______,                   _______,                            _______, _______, _______, _______
     )
 };
