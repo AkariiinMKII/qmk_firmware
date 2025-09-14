38b160c0026258e983e9cb62e9f0d183721b28b8
AkariiinMKII <6019344+AkariiinMKII@users.noreply.github.com>
2025-09-14 14:06:51 +0800

    Add RGB lighting

--- keyboards/akariiin_tech/akacube_hhkb/keymaps/via/keymap.c
+++ keyboards/akariiin_tech/akacube_hhkb/keymaps/via/keymap.c
@@ -3,6 +3,72 @@
 
 #include QMK_KEYBOARD_H
 
+// --- LED layout (0-7) ---:
+// LED 0-1 : CapsLock indicator (red)
+// LED 2-3 : Layer indicator
+// LED 4-7 : decorative RGB animations
+
+// Define half brightness HSV colors
+#define HSV_RED_HALF 0, 255, 128
+#define HSV_CYAN_HALF 128, 255, 128
+#define HSV_GREEN_HALF 85, 255, 128
+#define HSV_PURPLE_HALF 170, 255, 128
+#define HSV_MAGENTA_HALF 191, 255, 128
+
+// CapsLock -> LED 0-1
+const rgblight_segment_t PROGMEM user_layer_caps[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 2, HSV_RED_HALF}
+);
+
+// Layer -> LED 2-3
+const rgblight_segment_t PROGMEM user_layer_1[] = RGBLIGHT_LAYER_SEGMENTS(
+    {2, 2, HSV_CYAN_HALF}
+);
+
+const rgblight_segment_t PROGMEM user_layer_2[] = RGBLIGHT_LAYER_SEGMENTS(
+    {2, 2, HSV_GREEN_HALF}
+);
+
+const rgblight_segment_t PROGMEM user_layer_3[] = RGBLIGHT_LAYER_SEGMENTS(
+    {2, 2, HSV_PURPLE_HALF}
+);
+
+const rgblight_segment_t PROGMEM user_layer_4[] = RGBLIGHT_LAYER_SEGMENTS(
+    {2, 2, HSV_MAGENTA_HALF}
+);
+
+// Register layers.
+// Index 0 = user_layer_caps, 1 = user_layer_1, 2 = user_layer_2, etc.
+const rgblight_segment_t* const PROGMEM user_rgblight_layers[] = RGBLIGHT_LAYERS_LIST(
+    user_layer_caps,
+    user_layer_1,
+    user_layer_2,
+    user_layer_3,
+    user_layer_4
+);
+
+void keyboard_post_init_user(void) {
+    // enable layer system and default animation
+    rgblight_layers = user_rgblight_layers;
+    rgblight_enable();
+    rgblight_mode(RGBLIGHT_MODE_RAINBOW_MOOD); // MO(0)
+}
+
+// CapsLock change -> control layer index 0
+bool led_update_user(led_t led_state) {
+    rgblight_set_layer_state(0, led_state.caps_lock);
+    return true;
+}
+
+// Layer changes -> control layer indices 1 and 2
+layer_state_t layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(1, layer_state_cmp(state, 1)); // MO(1)
+    rgblight_set_layer_state(2, layer_state_cmp(state, 2)); // MO(2)
+    rgblight_set_layer_state(3, layer_state_cmp(state, 3)); // MO(3)
+    rgblight_set_layer_state(4, layer_state_cmp(state, 4)); // MO(4)
+    return state;
+}
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
@@ -29,20 +95,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
      * │Esc│F1 │F2 │F3 │F4 │F5 │F6 │F7 │F8 │F9 │F10│F11│F12│Ins│Del│
      * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤
-     * │Caps │ Q │ W │ E │ R │ T │ Y │ U │PSc│Scr│Pse│ ↑ │ ] │ Bspc│
+     * │Caps │Prv│Ply│Nxt│VlU│ T │ Y │ U │PSc│Scr│Pse│ ↑ │ ] │ Bspc│
      * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
-     * │ Ctrl │Vl-│Vl+│Mut│ F │ G │ * │ / │Hom│PgU│ ← │ → │  Enter │
+     * │ Ctrl │VAL│SAT│HUE│Mut│ G │ * │ / │Hom│PgU│ ← │ → │  Enter │
      * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤
-     * │ Shift  │ Z │ X │ C │ V │ B │ + │ - │End│PgD│ ↓ │ Shift│MO1│
+     * │ Shift  │RGB│NXT│SPD│VlD│ B │ + │ - │End│PgD│ ↓ │ Shift│MO1│
      * └─────┬──┴┬──┴──┬┴───┴───┴───┴───┴───┴───┴──┬┴───┴┬───┬─┴───┘
      *       │Alt│ GUI │                           │ GUI │Alt│
      *       └───┴─────┴───────────────────────────┴─────┴───┘
      */
     [1] = LAYOUT_60_hhkb(
         _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
-        KC_CAPS, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_UP,   _______, _______,
-        _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,          KC_PENT,
-        _______,          _______, _______, _______, _______, _______, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, _______, _______,
+        KC_CAPS, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU, _______, _______, _______, KC_PSCR, KC_SCRL, KC_PAUS, KC_UP,   _______, _______,
+        _______, UG_VALU, UG_SATU, UG_HUEU, KC_MUTE, _______, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,          KC_PENT,
+        _______,          UG_TOGG, UG_NEXT, UG_SPDU, KC_VOLD, _______, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, _______, _______,
                           _______, _______,                   _______,                            _______, _______
     )
 };
