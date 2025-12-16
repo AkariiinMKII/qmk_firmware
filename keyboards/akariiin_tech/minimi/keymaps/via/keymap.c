3006335512a5cbfbb7625394186068bebe9ea295
AkariiinMKII <6019344+AkariiinMKII@users.noreply.github.com>
2025-12-16 20:58:58 +0800

    Update keyboard configs
    Update keyboard.json

--- keyboards/akariiin_tech/minimi/keymaps/via/keymap.c
+++ keyboards/akariiin_tech/minimi/keymaps/via/keymap.c
@@ -44,48 +44,81 @@ void matrix_scan_user(void) {
     if (mnm_led_blink_timer_active()) { mnm_led_blink_timer(); }
 }
 
+// Keymap definitions
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    // Layer 0: Base QWERTY layout
+    // Layer 0: Base Alpha layout
     /*
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-     * │Esc│ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │ \ │ ` │
-     * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤
-     * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │ Bspc│
-     * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
-     * │ Ctrl │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │  Enter │
-     * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤
-     * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │ Shift│MO1│
-     * └─────┬──┴┬──┴──┬┴───┴───┴───┴───┴───┴───┴──┬┴───┴┬───┬─┴───┘
-     *       │GUI│ Alt │                           │ Alt │GUI│
-     *       └───┴─────┴───────────────────────────┴─────┴───┘
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+     * │ESC│ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │BKS│
+     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
+     * │TAB │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ENT  │
+     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
+     * │ SFT  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │SFT │
+     * └┬───┬─┴─┬─┴──┬┴──┬┴───┴───┴──┬┴──┬┴───┼───┼───┬┘
+     *  │CTL│GUI│ALT │MO1│  SPACE    │MO2│ALT │GUI│CTL│
+     *  └───┴───┴────┴───┴───────────┴───┴────┴───┴───┘
      */
-    [0] = LAYOUT_hhkb(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, MO(1),
-                          KC_LGUI, KC_LALT, KC_NO,            KC_SPC,  KC_NO,                    KC_RALT, KC_RGUI
+    [0] = LAYOUT_minimi(
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, MO(1),            KC_SPC,           MO(2),   KC_RALT, KC_RGUI, KC_RCTL
     ),
 
-    // Layer 1: Function keys and media controls
+    // Layer 1: Numeric and Symbols
     /*
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-     * │Esc│F1 │F2 │F3 │F4 │F5 │F6 │F7 │F8 │F9 │F10│F11│F12│Ins│Del│
-     * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┤
-     * │Caps │Prv│Ply│Nxt│VlU│ T │ Y │Num│PSc│Scr│Pse│ ↑ │ ] │ Bspc│
-     * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
-     * │ Ctrl │HUE│SAT│VAL│Mut│ G │ * │ / │Hom│PgU│ ← │ → │  Enter │
-     * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤
-     * │ Shift  │RGB│NXT│SPD│VlD│ B │ + │ - │End│PgD│ ↓ │ Shift│MO1│
-     * └─────┬──┴┬──┴──┬┴───┴───┴───┴───┴───┴───┴──┬┴───┴┬───┬─┴───┘
-     *       │GUI│ Alt │                           │ Alt │GUI│
-     *       └───┴─────┴───────────────────────────┴─────┴───┘
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+     * │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │DEL│
+     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
+     * │TAB │   │   │   │   │ - │ = │ [ │ ] │ \ │ ENT  │
+     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
+     * │ SFT  │   │   │   │   │   │ ; │ ' │ / │ ↑ │SFT │
+     * └┬───┬─┴─┬─┴──┬┴──┬┴───┴───┴──┬┴──┬┴───┼───┼───┬┘
+     *  │CTL│GUI│ALT │MO1│  SPACE    │MO2│  ← │ ↓ │ → │
+     *  └───┴───┴────┴───┴───────────┴───┴────┴───┴───┘
      */
-    [1] = LAYOUT_hhkb(
-        _______,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  KC_DEL,
-        KC_CAPS, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU, _______, _______, KC_NUM,  KC_PSCR, KC_SCRL, KC_PAUS, KC_UP,   _______, _______,
-        _______, UG_HUEU, UG_SATU, UG_VALU, KC_MUTE, _______, KC_PAST, KC_PSLS, KC_HOME, KC_PGUP, KC_LEFT, KC_RGHT,          KC_PENT,
-        _______,          UG_TOGG, UG_NEXT, UG_SPDU, KC_VOLD, _______, KC_PPLS, KC_PMNS, KC_END,  KC_PGDN, KC_DOWN, _______, _______,
-                          _______, _______, _______,          _______, _______,                   _______, _______
-    )
+     [1] = LAYOUT_minimi(
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+        KC_TAB,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, KC_ENT,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_SCLN, KC_QUOT, KC_SLSH, KC_UP,   KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, MO(1),            KC_SPC,           MO(2),   KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
+    // Layer 1: Shifted Numeric and Symbols
+    /*
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+     * │ ~ │ ! │ @ │ # │ $ │ % │ ^ │ & │ * │ ( │ ) │DEL│
+     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
+     * │CAPS│   │   │   │   │ _ │ + │ { │ } │ | │ ENT  │
+     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
+     * │ SFT  │   │   │   │   │   │ : │ " │ ? │ ↑ │SFT │
+     * └┬───┬─┴─┬─┴──┬┴──┬┴───┴───┴──┬┴──┬┴───┼───┼───┬┘
+     *  │CTL│GUI│ALT │MO1│  SPACE    │MO2│  ← │ ↓ │ → │
+     *  └───┴───┴────┴───┴───────────┴───┴────┴───┴───┘
+     */
+    [2] = LAYOUT_minimi(
+        KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
+        KC_CAPS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_ENT,
+        KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_COLN, KC_DQUO, KC_QUES, KC_UP,   KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, MO(3),            KC_SPC,           MO(2),   KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
+    // Layer 3: Function Keys
+    /*
+     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
+     * │ESC│F1 │F2 │F3 │F4 │   │   │PRT│SCR│PUS│   │   │
+     * ├───┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴───┤
+     * │    │F5 │F6 │F7 │F8 │   │   │INS│HOM│PGU│ ENT  │
+     * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
+     * │ SFT  │F9 │F10│F11│F12│   │   │DEL│END│PGD│SFT │
+     * └┬───┬─┴─┬─┴──┬┴──┬┴───┴───┴──┬┴──┬┴───┼───┼───┬┘
+     *  │CTL│GUI│ALT │MO1│  SPACE    │MO2│ALT │GUI│CTL│
+     *  └───┴───┴────┴───┴───────────┴───┴────┴───┴───┘
+     */
+    [3] = LAYOUT_minimi(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_NO,   KC_NO,   KC_PRT,  KC_SCR,  KC_PAU,  KC_NO,   KC_NO,
+        KC_NO,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_NO,   KC_NO,   KC_INS, KC_HOME, KC_PGUP, KC_ENT,
+        KC_LSFT, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_DEL,  KC_END,  KC_PGDN, KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, MO(3),            KC_SPC,           MO(2),   KC_RALT, KC_RGUI, KC_RCTL
+    ),
 };
