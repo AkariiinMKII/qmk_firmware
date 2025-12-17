1c73c13c0dc4f29ceaf320a353e4ee624caadc9f
AkariiinMKII <6019344+AkariiinMKII@users.noreply.github.com>
2025-12-18 01:55:26 +0800

    Update layer control

--- keyboards/akatech/minimi/keymaps/via/keymap.c
+++ keyboards/akatech/minimi/keymaps/via/keymap.c
@@ -93,14 +93,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
      * │ SFT  │   │   │   │   │   │ : │ " │ ? │ ↑ │SFT │
      * └┬───┬─┴─┬─┴──┬┴──┬┴───┴───┴──┬┴──┬┴───┼───┼───┬┘
-     *  │CTL│GUI│ALT │MO1│  SPACE    │MO2│  ← │ ↓ │ → │
+     *  │CTL│GUI│ALT │TG3│  SPACE    │MO2│  ← │ ↓ │ → │
      *  └───┴───┴────┴───┴───────────┴───┴────┴───┴───┘
      */
     [2] = LAYOUT_quark(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
         KC_CAPS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_ENT,
         KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_COLN, KC_DQUO, KC_QUES, KC_UP,   KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, MO(3),            KC_SPC,           MO(2),   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT, TG(3),            KC_SPC,           MO(2),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     // Layer 3: Function Keys
@@ -112,13 +112,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬────┤
      * │ SFT  │F9 │F10│F11│F12│   │   │DEL│END│PGD│SFT │
      * └┬───┬─┴─┬─┴──┬┴──┬┴───┴───┴──┬┴──┬┴───┼───┼───┬┘
-     *  │CTL│GUI│ALT │MO1│  SPACE    │MO2│ALT │GUI│CTL│
+     *  │CTL│GUI│ALT │TG3│  SPACE    │TG3│ALT │GUI│CTL│
      *  └───┴───┴────┴───┴───────────┴───┴────┴───┴───┘
      */
     [3] = LAYOUT_quark(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_NO,   KC_NO,   KC_PSCR, KC_SCRL, KC_PAUS, KC_NO,   KC_NO,
         KC_NO,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_NO,   KC_NO,   KC_INS,  KC_HOME, KC_PGUP, KC_ENT,
         KC_LSFT, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_DEL,  KC_END,  KC_PGDN, KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_LALT, MO(3),            KC_SPC,           MO(2),   KC_RALT, KC_RGUI, KC_RCTL
+        KC_LCTL, KC_LGUI, KC_LALT, TG(3),            KC_SPC,           TG(3),   KC_RALT, KC_RGUI, KC_RCTL
     ),
 };
